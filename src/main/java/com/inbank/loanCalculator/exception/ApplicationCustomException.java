5bcfe479775feb945ea2403564e33ada1b4a508d
Abdelsalam <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2023-02-02 13:43:10 +0100

    ApplicationCustomException

---
+++ src/main/java/com/inbank/loanCalculator/exception/ApplicationCustomException.java
@@ -0,0 +1,7 @@
+package com.inbank.loanCalculator.exception;
+
+public class ApplicationCustomException extends Exception {
+    public ApplicationCustomException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/inbank/loanCalculator/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.inbank.loanCalculator.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ApplicationCustomException.class)
+    public Map<String, String> handleCustomException(ApplicationCustomException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("error", exception.getMessage());
+
+        return errorMap;
+    }
+}
