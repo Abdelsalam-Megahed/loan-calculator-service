9ce3f43d910982450304f13b3eb18ec39e832e1f
Abdelsalam <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2023-02-02 13:43:37 +0100

    LoanCalculatorRequest validation

--- src/main/java/com/inbank/loanCalculator/LoanCalculatorRequest.java
+++ src/main/java/com/inbank/loanCalculator/LoanCalculatorRequest.java
@@ -1,5 +1,8 @@
 package com.inbank.loanCalculator;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoanCalculatorRequest {
+    @NotBlank(message = "Personal code is invalid!")
     private String personalCode;
+    @Min(2000)
+    @Max(10000)
     private float loanAmount;
+    @Min(12)
+    @Max(60)
     private int loanPeriod;
 }
